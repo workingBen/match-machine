97c49bace0e6141b739da27efd605f22c7e3ec65
Ben Pellow <ben@campuscred.com>
2012-01-11 15:19:28 -0800

    root route to home/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 MatchMachine::Application.routes.draw do
-  get "home/index"
-
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -52,7 +50,7 @@ MatchMachine::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root to: 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
