c4e861165efb09ac4dfd738fe622efaa5bc9c156
Ben Pellow <ben@campuscred.com>
2012-01-11 14:20:49 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MatchMachine::Application.config.secret_token = 'afcc7944345208a33b5b9f4eeb9f70776a324a920ab204840cf9d46e9c60f2fe6c4cce60dcb8b57b466802c2c4ed7cbdc790131d4513e94319fa1e6d00962b39'
