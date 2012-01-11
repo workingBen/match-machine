64d6dc08911c2a97cd06d9c859d7fe950f972c35
Ben Pellow <ben@campuscred.com>
2012-01-11 15:13:01 -0800

    add validations for username, email and make username attr_accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  
+  # VALIDATIONS
+  validates_presence_of :username
+  validates_uniqueness_of :username, :email, case_sensitive: false
 end
