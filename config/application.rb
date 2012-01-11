5221adea19e168d3a4c2b73c63a46d91aa210bd7
Ben Pellow <ben@campuscred.com>
2012-01-11 14:59:25 -0800

    add filter params for password, password_confirmation

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,7 @@ module MatchMachine
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
 
     # Enable the asset pipeline
     config.assets.enabled = true
