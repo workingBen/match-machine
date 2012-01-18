58b6a63813b292a1251d42bd18b4379f781cef7b
Ben Pellow <benjamin.pellow@gmail.com>
2012-01-18 13:01:55 -0800

    add message association to user

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ActiveRecord::Base
+  belongs_to :user
+
   def self.new_from_template(user)
     template = user.message_templates.first || MessageTemplate.new
     Message.new(subject: template.subject, message: template.message, user_id: user.id)
